a3e4957cb01d5f43ea3dfe7c3b8f1d2e8d1498c9
Brynjar <binnibja@gmail.com>
2023-09-06 11:10:35 +0000

    project layout fix

--- lib/stimpilklukka_backend_web/controllers/task_controller.ex
+++ lib/stimpilklukka_backend_web/controllers/task_controller.ex
@@ -2,10 +2,12 @@ defmodule StimpilklukkaBackendWeb.TaskController do
   use StimpilklukkaBackendWeb, :controller
 
   alias StimpilklukkaBackend.Tasks
+  alias StimpilklukkaBackend.Projects
 
   def index(conn, params) do
     project_id = params["project_id"]
+    project = Projects.get_project!(project_id)
     tasks = Tasks.list_tasks_by_projectid(project_id)
-    render(conn, :index, tasks: tasks)
+    render(conn, :index, tasks: tasks, project: project, layout: {StimpilklukkaBackendWeb.Layouts, "project"})
   end
 end
--- lib/stimpilklukka_backend_web/controllers/time_entry_controller.ex
+++ lib/stimpilklukka_backend_web/controllers/time_entry_controller.ex
@@ -1,11 +1,13 @@
 defmodule StimpilklukkaBackendWeb.TimeEntryController do
   use StimpilklukkaBackendWeb, :controller
 
+  alias StimpilklukkaBackend.Projects
   alias StimpilklukkaBackend.TimeEntries
 
   def index(conn, params) do
      project_id = params["project_id"]
      time_entries = TimeEntries.list_time_entries_by_projectid(project_id)
-     render(conn, :index, time_entries: time_entries)
+     project = Projects.get_project!(project_id)
+     render(conn, :index, time_entries: time_entries, project: project, layout: {StimpilklukkaBackendWeb.Layouts, "project"})
   end
 end
